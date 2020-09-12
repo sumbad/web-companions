48c7f19f838299cf64ce2c3adbf366fa139f2c4b
Denis Sumbaev <denis.sumbaev@gmail.com>
2020-09-13 01:18:56 +0800

    feat: the engine for virtual function

--- packages/fc/demo/src/index.tsx
+++ packages/fc/demo/src/index.tsx
@@ -1,7 +1,10 @@
 import { FC } from '@web-companions/fc';
-import { useState, useEffect, useCallback, useRef } from 'augmentor';
+// import { useState, useEffect, useRef, useCallback, augmentor  as $} from 'augmentor';
+import { augmentor as $, useEffect, useRef, useState, useCallback } from 'dom-augmentor';
 import { loadingProgressBar } from './loadingProgressBar';
-// import { html } from 'uhtml';
+// import { html, render } from 'uhtml';
+import { html as $html, render } from 'uhtml';
+const html = (...args) => $html.for(useRef(null)).apply(null, args);
 
 // FC(
 //   {
@@ -51,8 +54,6 @@ import { loadingProgressBar } from './loadingProgressBar';
 //   setTimeout(() => setCount(count + 1), 1000);
 // }
 
-
-
 // import { useState, useRef, useCallback } from 'haunted';
 // import { FC } from '@web-companions/fc';
 // import { loadingProgressBar } from './loadingProgressBar';
@@ -82,18 +83,71 @@ import { loadingProgressBar } from './loadingProgressBar';
 //   );
 // }).virtual();
 
-// const CounterVirtualAsElement = FC((prop: { msg: string }) => {
-//   const [count, setCount] = useState(0);
+const CounterVirtualAsElement = FC(
+  {
+    msg: String,
+  },
+  (prop) => {
+    const [count, setCount] = useState(0);
 
-//   return (
-//     <button type="button" onclick={() => setCount(count + 1)}>
-//       {count} {prop.msg}
-//     </button>
-//   );
-// }).virtual();
+    // console.log('CounterVirtualAsElement', count);
+
+    // setTimeout(() => {
+    //   // console.log(count);
+
+    //   setCount(count + 1);
+    // }, 1000);
+
+    return html`
+      <>
+        <button type="button" onclick=${() => setCount(count + 1)}>
+          ${prop.msg}
+        </button>
+        <i>${count}</i>
+      </>
+    `;
+  }
+);
+
+//.virtual();
+
+
+
+
+const Button = () => $((text) => {
+  useEffect(
+    () => {
+      console.log('connected');
+      return () => console.log('disconnected');
+    },
+    []
+  );
+  const [i, increment] = useState(0);
+  
+  // console.log(1111);
+  
+  return html`
+  <button onclick=${() => increment(i + 1)}>
+    ${text} ${i}
+  </button>`;
+});
+
+const button = Button();
+// const button2 = Button('22222');
+
+// render(document.body, button);
+
+
+
+
+
 
 const LoadingProgressBarEl = loadingProgressBar.element('loading-progress-bar');
 
+const h1 = document.createElement('div');
+// const h2 = document.createElement('div');
+// const CounterVirtualAsElement1 = CounterVirtualAsElement.virtual();
+const CounterVirtualAsElement2 = CounterVirtualAsElement.virtual();
 /**
  * ROOT element
  */
@@ -107,19 +161,48 @@ FC(() => {
     }
   }, []);
 
-  console.log(111111, myRef);
+  // console.log(111111, CounterVirtualAsElement({ msg: '1111' }));
+  // a();
 
   return (
     <>
       <LoadingProgressBarEl ref={myRef}></LoadingProgressBarEl>
       <button onclick={handleProgress}>Progress loading</button>
-      <h1>Hello!</h1>
+      {/* <h1>Hello!</h1> */}
       {/* <CounterEl head="Counter"></CounterEl> */}
       <br />
       <br />
       {/* <div>{CounterVirtualAsExpression({ msg: 'Virtual Counter as Expression' })}</div> */}
       {/* <br /> */}
-      {/* <CounterVirtualAsElement msg="Virtual Counter as Element"></CounterVirtualAsElement> */}
+      {/* { CounterVirtualAsElement1({ msg: 'sdfsdf' }) } */}
+
+      {/* <CounterVirtualAsElement2 msg="Virtual Counter as Element"></CounterVirtualAsElement2> */}
+
+      {/* {true ? CounterVirtualAsElement2({ msg: 'a', id: 'a' }) : undefined} */}
+
+      {/* { button('11111') }
+      { button('33333') } */}
+     
+      { Button()('11111') }
+      { Button()('22222') }
+
+      {CounterVirtualAsElement2({ msg: 'aaaaa' })}
+      {CounterVirtualAsElement2({ msg: 'bbbbb' })}
+
+      {/* <div>
+        {[
+          { id: '1', name: 1 },
+          { id: '2', name: 2 },
+          { id: '3', name: 3 },
+        ].map(
+          ({ id, name }) =>
+            html`<li data-id=${id}>${name}</li>
+              ${CounterVirtualAsElement2({ msg: id, id })}`
+        )}
+      </div> */}
     </>
   );
 }).element('demo-fc');
+
+
+
--- packages/fc/src/index.ts
+++ packages/fc/src/index.ts
@@ -2,8 +2,9 @@
 // import type { Component } from 'haunted/lib/component';
 // import type { ElementOptions } from './index.type';
 
-import { augmentor } from 'augmentor';
-import { render, Renderable } from 'uhtml';
+import { augmentor, useRef } from 'dom-augmentor';
+// import { augmentor } from 'augmentor';
+import { html, render, Renderable } from 'uhtml';
 
 function renderFunc(container: Element | ShadowRoot | DocumentFragment, template: Renderable) {
   render(container, template);
@@ -28,13 +29,9 @@ interface ElementConfig<P> {
 //     [x in keyof P]: ElementConfigProp<P[x]> | null;
 // };
 
-
 // export interface ComponentFunc<P> {
 //   (props: P): unknown | void;
 // }
-
-
-
 
 function reflectAttrFromProp(el: Element, attrName: string, value: any) {
   if (el instanceof Element) {
@@ -52,7 +49,6 @@ function reflectAttrFromProp(el: Element, attrName: string, value: any) {
     }
   }
 }
-
 
 export function defineElement<P, EP>(name: string, func: ComponentFunc<P>, elProps?: EP, config?: ElementConfig<P>) {
   let _mapper = function <T extends keyof P>(props: P, key: T | any, value: any): P {
@@ -112,8 +108,8 @@ export function defineElement<P, EP>(name: string, func: ComponentFunc<P>, elPro
 
         func = new Proxy(func, {
           apply: (target, thisArg, args) => {
-            console.log(333333, this.connected, target, thisArg, args);
             const temp = Reflect.apply(target, this, args);
+            // console.log(333333, temp, this.connected, target, thisArg, args);
             if (this.connected) {
               renderFunc(container, temp);
             }
@@ -203,11 +199,10 @@ export function defineElement<P, EP>(name: string, func: ComponentFunc<P>, elPro
   }
 }
 
-
 export function createElement<P, EP>(name: string, func: ComponentFunc<P>, elProps?: EP, config?: ElementConfig<any>) {
   defineElement(name, func, elProps, config);
 
-  const createInstance = async (properties?: P & Partial<HTMLElement> & {ref: {}}) => {
+  const createInstance = async (properties?: P & Partial<HTMLElement> & { ref: {} }) => {
     const elClass = await customElements.whenDefined(name).then(() => customElements.get(name));
     // TODO: need check, as I can see it doesn't work
     if (typeof properties === 'object') {
@@ -223,18 +218,13 @@ export function createElement<P, EP>(name: string, func: ComponentFunc<P>, elPro
 
   return createInstance;
 }
-  
 
-const foo = <T extends string | number>(
-  first: T, 
-  ...prop: (T extends string ? [T] : [])
-) => undefined;
+const foo = <T extends string | number>(first: T, ...prop: T extends string ? [T] : []) => undefined;
 
 foo('1', '1');
 foo(1);
 
-type m<A> = {m: A};
-
+type m<A> = { m: A };
 
 export type ElementConfigProp<G> = {
   attribute?: string;
@@ -248,32 +238,35 @@ export type ElementProperties<P> = {
   [x in keyof P]: ElementConfigProp<P[x]> | StringConstructor | BooleanConstructor | NumberConstructor | ArrayConstructor;
 };
 
-
 type a = ReturnType<typeof String>;
 
 type Diff<T, U> = T extends U ? never : T;
 
-export type defProp1<EP> = {[K in keyof EP]: EP[K] extends StringConstructor ? ReturnType<EP[K]> : EP[K] extends ElementConfigProp<any> ? EP[K]['init'] : unknown;};
+export type defProp1<EP> = {
+  [K in keyof EP]: EP[K] extends StringConstructor ? ReturnType<EP[K]> : EP[K] extends ElementConfigProp<any> ? EP[K]['init'] : unknown;
+};
 // export type defProp<EP> = Partial<{[K in keyof EP]: EP[K] extends StringConstructor ? ReturnType<EP[K]> : EP[K] extends ElementConfigProp<any> ? EP[K]['init'] : unknown;}>;
 // export type defProp1<EP> = {[K in keyof EP]: EP[K] extends StringConstructor ? ReturnType<EP[K]> : EP[K] extends ElementConfigProp<any> ? (EP[K]['init'] extends undefined ? unknown : EP[K]['init']) : unknown;};
 // export type defProp2<EP> = {[K in keyof EP]: EP[K] extends StringConstructor ? ReturnType<EP[K]> : EP[K] extends ElementConfigProp<any> ? (EP[K]['init'] extends undefined ? EP[K]['init'] : unknown) : unknown;};
-type AtLeastOne<T, U = {[K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U];
-type RequireOnlyOne<T> =
-    Pick<T, Exclude<keyof T, keyof {[L in keyof T]: T[L] extends object ? T[L] : never}>>;
-    // & {
-    //     [K in Keys]-?:
-    //         Required<Pick<T, K>>
-    //         & Partial<Record<Exclude<Keys, K>, undefined>>
-    // }[Keys]
+type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U];
+type RequireOnlyOne<T> = Pick<T, Exclude<keyof T, keyof { [L in keyof T]: T[L] extends object ? T[L] : never }>>;
+// & {
+//     [K in Keys]-?:
+//         Required<Pick<T, K>>
+//         & Partial<Record<Exclude<Keys, K>, undefined>>
+// }[Keys]
 // export type defProp<EP> = AtLeastOne<defProp1<EP>, {[K in keyof defProp1<EP>]: defProp1<EP>[K] extends undefined ? defProp1<EP>[K]: never}>;
 type Optional<T, K extends keyof T> = Omit<T, K> & Partial<T>;
 // export type defProp<EP> = Optional<defProp1<EP>, ({[I in keyof EP]: EP[I] extends ElementConfigProp<unknown | undefined> ? I : never})[keyof EP]>;//RequireOnlyOne<defProp1<EP>>;
 type Filter<T, U> = T extends U ? T : never;
-export type defProp<EP> = Optional<defProp1<EP>, ({[K in keyof defProp1<EP>]: Filter<defProp1<EP>[K], undefined> extends never ? never : K})[keyof defProp1<EP>]>;//RequireOnlyOne<defProp1<EP>>;
+export type defProp<EP> = Optional<
+  defProp1<EP>,
+  { [K in keyof defProp1<EP>]: Filter<defProp1<EP>[K], undefined> extends never ? never : K }[keyof defProp1<EP>]
+>; //RequireOnlyOne<defProp1<EP>>;
 // ({[I in keyof defProp1<EP>]: defProp1<EP>[I] extends (defProp1<EP>[I] & undefined) ? I : never})[keyof EP]
 // type ComponentFunc<P, EP> = (props: P & {[K in keyof EP]: EP[K] extends StringConstructor ? ReturnType<EP[K]> : never;}) => unknown | void;
 export type ComponentFunc<P> = (props: P) => unknown | void;
-
+export type ComponentFuncWithoutParams = () => unknown | void;
 
 // type ttt = ComponentFunc<{a: }, {a: StringConstructor}>
 
@@ -290,8 +283,15 @@ export type ComponentFunc<P> = (props: P) => unknown | void;
 // export function defineElement1<P>(...prop:  P extends object ? [ElementProperties<P>, (props: P) => unknown | void] : [(props: P) => unknown | void]) {}
 // export function defineElement1<P>(props: ElementProperties<P> | ComponentFunc<ElementProperties<P>>, ...args:  P extends object ? [ComponentFunc<ElementProperties<P>>, ElementConfig<P>?] : [ElementConfig<P>?]) {}
 // export function defineElement1<P, PP extends ElementProperties<P> | ComponentFunc<unknown>>(props: PP, ...args: PP extends ComponentFunc<unknown> ? [ElementConfig<P>?] : [ComponentFunc<PP>, ElementConfig<P>?]) {}
-export function defineElement1<P, PP extends ElementProperties<P> | ComponentFunc<P> = ElementProperties<P> | ComponentFunc<P>>(props: PP, ...args: PP extends Function ? [ElementConfig<P>?] : [ComponentFunc<P extends object ? P : defProp<PP>>, ElementConfig<P>?]) {}
-
+export function defineElement1<
+  P,
+  PP extends ElementProperties<P> | ComponentFuncWithoutParams = ElementProperties<P> | ComponentFuncWithoutParams
+>(
+  props: PP,
+  ...args: PP extends ComponentFuncWithoutParams
+    ? [ElementConfig<P>?]
+    : [ComponentFunc<P extends object ? P : defProp<PP>>, ElementConfig<P>?]
+) {}
 
 // type mff = Filter<string | undefined | null, undefined | string>;
 // type mff<EP> = ({[K in keyof EP]: Filter<EP[K], undefined> extends never ? never : K})[keyof EP];
@@ -305,62 +305,181 @@ export function defineElement1<P, PP extends ElementProperties<P> | ComponentFun
 // const bbb: aa;
 // bbb
 
-
 defineElement1(
   {
     a: String,
     b: {
-        init: '' as string | undefined
-      }
+      init: '' as string | undefined,
+    },
   },
-  function aaa (props) {
+  function aaa(props) {
     console.log(props);
   }
-)
+);
 
-defineElement1<{b?: string}>(
-// defineElement1(
+defineElement1<{ b?: string }>(
+  // defineElement1(
   {
-    b: {init: '1'}
+    b: { init: '1' },
   },
-  function aaa (props) {
+  function aaa(props) {
     console.log(props.b);
   }
-)
-
-
-defineElement1(
-  function () {
-      console.log();
-  }
 );
 
-defineElement1(
-    () => {
-      console.log();
-  }
-);
+defineElement1(function () {
+  console.log();
+});
 
+defineElement1(() => {
+  console.log();
+});
 
-const aa = String();
-
-
-
-
-export function FC<P, PP extends ElementProperties<P> | ComponentFunc<P> = ElementProperties<P> | ComponentFunc<P>>(props: PP, ...args: PP extends Function ? [ElementConfig<P>?] : [ComponentFunc<P extends object ? P : defProp<PP>>, ElementConfig<P>?]) {
-  let func: any//ComponentFunc<unknown | P extends object ? P : defProp<PP>>;
+export function FC<P, PP extends ElementProperties<P> | ComponentFuncWithoutParams = ElementProperties<P> | ComponentFuncWithoutParams>(
+  props: PP,
+  ...args: PP extends ComponentFuncWithoutParams
+    ? [ElementConfig<P>?]
+    : [ComponentFunc<P extends object ? P : defProp<PP>>, ElementConfig<P>?]
+) {
+  let func: any; //ComponentFunc<unknown | P extends object ? P : defProp<PP>>;
   let elProps: PP;
   const defaultElementConfig = args[1];
-  
+
   // TODO: remove types casting
-  if(typeof props === 'function') {
+  if (typeof props === 'function') {
     func = props as ComponentFunc<unknown>;
   } else if (typeof args[0] === 'function') {
     elProps = props;
     func = args[0];
   }
+
+
+
+
   return {
-    element: (name: string, config: ElementConfig<P> | undefined = defaultElementConfig) => createElement<P, PP>(name, func, elProps, config),
-    virtual: () => augmentor(func),
+    element: (name: string, config: ElementConfig<P> | undefined = defaultElementConfig) =>
+      createElement<P, PP>(name, func, elProps, config),
+
+    virtual: () => {
+      const nodeFuncMap = new WeakMap();
+      let count = 0;
+      const $html = (props: any) => {
+        const currentNode = useRef(null);
+        let currentFunc = nodeFuncMap.get(currentNode);
+        if(currentFunc !== undefined) {
+          
+        } else {
+          currentFunc = augmentor(func);
+          nodeFuncMap.set(currentNode, currentFunc);
+        }
+        
+        // console.log(nodes, nodes.has(useRef(null)));
+
+        // const m = useRef(null);
+        // count = count + 1;
+        // aaa.set(m, count);
+        // console.log('args ', args);
+        
+        return html.for(currentNode)`${currentFunc(props)}`//.apply(null, [aFunc(props)]);
+      };
+
+      // const host = html.node`<div />`;
+      // console.log('11111111 virtual', host.firstChild);
+      return function (props: P) {
+
+        return $html(props); //`${aFunc(props)}`;
+      };
+    },
   };
 }
+
+// --------------------------------------------------------------- (3)
+
+// console.log(host, props.id, host.firstChild);
+
+// let element = host.querySelector('#virtual-' + props.id);
+// let aFunc;
+
+// console.log(props.id, element, host.firstChild);
+// if (element === null || element['aFunc'] === undefined) {
+//   element = document.createElement('div');
+//   element.setAttribute('id', 'virtual-' + props.id);
+//   host.appendChild(element);
+
+// const _func = func.bind(element);
+// const pFunc = new Proxy(_func, {
+//   apply: (target, thisArg, args) => {
+//     // console.log(999, target, thisArg, args);
+//     const temp = Reflect.apply(target, thisArg, args);
+//     render(element as Element, temp);
+//     return temp;
+//   },
+// });
+// aFunc = augmentor(func);
+//   element['aFunc'] = aFunc;
+// } else {
+//   aFunc = element['aFunc'];
+// }
+
+// console.log(host.querySelector('#virtual-a'), host.querySelector('#virtual-b'));
+
+// aFunc(props);
+
+//----------------------------------------------(2)
+
+// virtual: (host: Node = document.createElement('div')) => {
+//   const _func = func.bind(host);
+
+//   const pFunc = new Proxy(_func, {
+//     apply: (target, thisArg, args) => {
+//       // console.log(999, target, thisArg, args);
+//       const temp = Reflect.apply(target, thisArg, args);
+//       render(host, temp);
+//       // render(host, html.for(host, props.id)`${temp}`);
+//       return temp;
+//     },
+//   });
+//   const aFunc = augmentor(pFunc);
+
+//   return function (props: P) {
+//     aFunc(props);
+//     return host;
+//   };
+// },
+
+//----------------------------------------------(1)
+
+// virtual: () => {
+//   let hosts = new WeakMap();
+
+//   return function (props: P & { host: Node }) {
+//     console.log(4444, hosts, hosts.has(props.host));
+//     console.log(2222, props);
+
+//     if (hosts.has(props.host)) {
+//       const host = hosts.get(props.host);
+//       host.func(props);
+//       return host.host;
+//     } else {
+//       // const host = document.createElement('div');
+//       // console.log('virt-host', host);
+//       const host = props.host;
+//       const _func = func.bind(host);
+
+//       const pFunc = new Proxy(_func, {
+//         apply: (target, thisArg, args) => {
+//           // console.log(999, target, thisArg, args);
+//           const temp = Reflect.apply(target, thisArg, args);
+//           render(host, temp);
+//           // render(host, html.for(host, props.id)`${temp}`);
+//           return temp;
+//         },
+//       });
+//       const aFunc = augmentor(pFunc);
+//       hosts.set(host, { func: aFunc, host });
+
+//       aFunc(props);
+//       return host;
+//     }
+//   };
+// },
