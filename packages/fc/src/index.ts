eef19d204948f8671551172528f816b69de9eef5
Denis Sumbaev <denis.sumbaev@gmail.com>
2020-10-01 12:33:44 +0800

    feat: export hooks

--- packages/fc/src/index.ts
+++ packages/fc/src/index.ts
@@ -1,2 +1,4 @@
 export { EFC } from './EFC';
 export { VFC } from './VFC';
+
+export { createContext, useCallback, useContext, useEffect, useLayoutEffect, useMemo, useReducer, useRef, useState } from 'augmentor';
