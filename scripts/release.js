fd4994be85906ece883cca3ff8787c265820e5d1
Denis Sumbaev <denis.sumbaev@gmail.com>
2021-02-10 17:40:21 +0800

    handle exceptions from the release script

--- scripts/release.js
+++ scripts/release.js
@@ -4,12 +4,16 @@ const { execSync, spawn } = require('child_process');
 console.log(`> npx lerna bootstrap`);
 execSync('npx lerna bootstrap', { stdio: 'inherit', shell: true });
 
-// Testing and building packages before publish
-const changedPackages = JSON.parse(execSync('npx lerna changed --include-merged-tags --json', { encoding: 'utf8' }));
-const packagesScope = changedPackages.map((p) => `--scope="${p.name}"`).join(' ');
-const testPackagesCmd = `npx lerna exec --stream ${packagesScope} "npm test --if-present && npm run build --if-present"`;
-console.log(`> ${testPackagesCmd}`);
-execSync(testPackagesCmd, { stdio: 'inherit', shell: true });
+try {
+  // Testing and building packages before publish
+  const changedPackages = JSON.parse(execSync('npx lerna changed --include-merged-tags --json', { encoding: 'utf8' }));
+  const packagesScope = changedPackages.map((p) => `--scope="${p.name}"`).join(' ');
+  const testPackagesCmd = `npx lerna exec --stream ${packagesScope} "npm test --if-present && npm run build --if-present"`;
+  console.log(`> ${testPackagesCmd}`);
+  execSync(testPackagesCmd, { stdio: 'inherit', shell: true });
+} catch (error) {
+  console.warn(`Error: ${error.message}\n`);
+}
 
 // Publish packages
 const branchName = execSync('git symbolic-ref --short HEAD', { encoding: 'utf8' }).trim();
@@ -25,3 +29,9 @@ const cmd =
       )} --no-push ${process.argv.slice(2).join(' ')}`;
 console.log(`> ${cmd}`);
 spawn(cmd, { stdio: 'inherit', shell: true });
+
+
+// Publish only certain packages
+// npx lerna publish --include-merged-tags --conventional-graduate @web-companions/fc,@web-companions/react-adapter
+// or
+// npm run release -- @web-companions/fc,@web-companions/react-adapter
